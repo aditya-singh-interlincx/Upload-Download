80f725f9d84fa4070728533bffdb48d224fb64c8
Aditya Singh <adityasorot@gmail.com>
2023-09-25 23:24:02 +0530

    upload download multer

---
+++ routes.js
@@ -0,0 +1,25 @@
+const appRoot = require('app-root-path')
+const multer = require('multer')
+const fs = require('fs')
+
+const fileController = require(`${appRoot}/src/controllers/FileController.js`)
+const INPUT_NAME = 'photos'
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    const path = 'files'
+    fs.mkdirSync(path, { recursive: true })
+    cb(null, path)
+  },
+  filename: (req, file, cb) => {
+    cb(null,file.originalname)
+  }
+})
+
+module.exports = (app) => {
+  app.use(multer({
+    storage
+  }).single('file'))
+  app.post('/upload', fileController.upload)
+  app.post('/download', fileController.download)
+} 
